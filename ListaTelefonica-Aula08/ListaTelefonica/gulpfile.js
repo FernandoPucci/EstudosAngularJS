d13c04986983da19801f7e138b2f5a8cbbdb869e
FernandoPucci <fsilvapucci@gmail.com>
2016-10-29 12:54:37 -0200

    Aula 07 e 08 mais aula Gulp

---
+++ ListaTelefonica-Aula08/ListaTelefonica/gulpfile.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint'); //jshint avalia possiveis erros no código JS
+var clean = require('gulp-clean'); //limpa o projeto
+var concat = require('gulp-concat'); //concatena os codigos
+var uglify = require('gulp-uglify');
+var es = require('event-stream');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+
+var runSequence = require('run-sequence');
+var rename = require('gulp-rename');
+
+//se não se colocar a clausula 'return' as tasks serão executadadas de forma sincrona
+ 
+gulp.task('clean', function(){
+		return gulp.src('dist/')
+		.pipe(clean());
+
+});
+
+gulp.task('jshint', function(){
+	//onde eh feita a leitura dos arquivos
+		return gulp.src('js/**/*.js') //readable Stream
+		.pipe(jshint()) //envio para jshint
+		.pipe(jshint.reporter('default')); //renderizo um report
+});
+
+gulp.task('uglify', function(){ 
+		return es.merge([
+			gulp.src(['lib/angular/angular.min.js','lib/angular/angular-messages.min.js', 'lib/angular/angular-locale_pt-br.js']), //estes precisam entrar no build mas nao precisam uglifycar, pois já estão minificados
+			// gulp.src(['js/*.js','js/controllers/*.js']).pipe(concat('scripts.js')).pipe(uglify())
+			gulp.src(['js/**/*.js']).pipe(concat('scripts.js'))//.pipe(uglify())
+		])		
+		.pipe(concat('all.min.js'))
+		.pipe(gulp.dest('dist/js'));
+
+});
+
+//minificação da html
+gulp.task('htmlmin', function(){ 
+
+	return gulp.src(['*.html','view/*.html'])
+	.pipe(htmlmin({collapseWhitespace: true}))	
+	.pipe(gulp.dest('dist/view'))
+
+});
+
+//minificação de css
+gulp.task('cssmin', function(){
+
+	return gulp.src(['lib/bootstrap/**/*.css', 'css/**/*.css'])
+	.pipe(cleanCSS())
+	.pipe(concat('styles.min.css'))
+	.pipe(gulp.dest('dist/css'));
+
+
+})
+
+//copia o index para a dist
+gulp.task('copy', function(){
+
+	return gulp.src('index-prod.html')
+	.pipe(rename('index.html'))
+	.pipe(gulp.dest('dist/'));
+
+
+});
+
+//executa as tasks
+gulp.task('default', function(cb){
+
+	return runSequence('clean', ['jshint', 'uglify', 'htmlmin', 'cssmin', 'copy'], cb)
+
+}); 	
---
+++ ListaTelefonica-Aula08/ListaTelefonica/js/controllers/listaTelefonicaController.js
@@ -0,0 +1,134 @@
+angular.module("listaTelefonica").controller("listaTelefonicaController", function($scope, $http){
+
+			$scope.app = "Lista Telefonica";
+
+
+			$scope.cores = [
+			{nome:"Azul", codigo:"blue"},
+			{nome:"Amarelo", codigo:"yellow"},
+			{nome:"Vermelho", codigo:"red"},
+			{nome:"Verde", codigo:"green"},
+			{nome:"Magenta", codigo:"magenta"}
+			];
+
+			$scope.cidades=[];
+			$scope.contatos=[];
+
+			//TODO: criar via JSON um array de estados
+			var carregarArrayComboEstados = function(){
+
+				$http.get("http://localhost:8089/WebFuelServices/api/utils/listAllEstados").success(function(data, status){
+
+					//crio o array estados, baseado nos dados dos estados retornados do BackEnd
+					$scope.estados = data;
+
+
+				});
+
+
+			};
+
+			$scope.carregarArrayComboCidades = function(estado){
+
+				if(estado!==null){
+					$http.get("http://localhost:8089/WebFuelServices/api/utils/listAllCidadesByIdEstado?idEstado=" + estado).success(function(data, status){
+						console.log(data);
+						console.log(status);
+						$scope.cidades = data;
+
+					}).error(function(data, status){
+						if(status != 200){
+
+							$scope.cidades = [];
+						}
+
+					});
+				}
+			};
+
+			var carregarContatos = function () {
+				$http.get("http://localhost:3412/contatos").success(function (data) {
+					$scope.contatos = data;
+				}).error(function (data, status) {
+					$scope.message = "Aconteceu um problema: " + data;
+				});
+			};
+
+			var carregarOperadoras = function () {
+				$http.get("http://localhost:3412/operadoras").success(function (data) {
+					$scope.operadoras = data;
+				});
+			};
+
+			//verifica se o array de cidades esta preenchido
+			$scope.checaCidades = function(){
+
+				if($scope.cidades.length > 1){
+					return true;
+				}else{
+					return false;
+				}
+
+			};
+
+			$scope.adicionarContato = function(contato){
+				contato.data = new Date();
+
+
+				
+				$http.post("http://localhost:3412/contatos", contato).success(function (data) {
+					delete $scope.contato;
+					$scope.contatoForm.$setPristine();
+					carregarContatos();
+				});
+
+
+			  /*  $scope.contatos.push(angular.copy(contato)); //copia o contato para a lista, para não alterá-la
+			    delete $scope.contato; //limpa os campos do contato
+			    $scope.contatoForm.$setPristine();
+			    */
+			    $scope.estados = [];
+
+			    carregarArrayComboEstados();
+			    $scope.cidades = [];
+
+			};
+
+			$scope.apagarContatos = function(contatos){
+				
+				$scope.contatos = contatos.filter(function(contato){ //devolvo ao scope os contatos que
+
+						if(!contato.selecionado){ //nao estao selecionados
+							return contato;
+						}
+
+					});			
+			};
+
+			//funcao para ordenacao
+			$scope.ordernarPor  = function(campo){
+				//console.log(campo);
+				$scope.criterioDeOrdenacao = campo;
+				$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+			};
+
+			$scope.isContatoSelecionado = function(contatos){
+
+					return contatos.some(function(contato){ //se algum contato estiver selecionado retorna true
+
+						return contato.selecionado;
+
+					});
+
+				};
+
+				$scope.classe1 = "selecionado";
+				$scope.classe2 = "negrito";
+
+				carregarContatos();
+				carregarOperadoras();
+				carregarArrayComboEstados();
+
+			});
+
+
